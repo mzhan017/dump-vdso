d5da6b530c1210d6c28f94e90ade3199e93a4f50
mzhan017 <420075746@qq.com>
2026-02-01 06:17:58 +0800

    Add support for 32bit

--- README.md
+++ README.md
@@ -2,15 +2,20 @@
 
 ## What
 
-Dumps the Linux VDSO page to standard output.
+Dumps the Linux VDSO page to standard output. The vDSO (virtual dynamic shared object) is a shared library that the kernel automatically maps into the address space of all user-space applications. It contains implementations of certain system calls that can be executed in user space, which can improve performance by avoiding the overhead of a traditional system call.
 
 ## Who
 
 Written by Geoffrey Thomas as part of [this blog post](https://ldpreload.com/blog/stupid-tricks-at-the-userspace-kernelspace-boundary) and [released](https://ldpreload.com/pages/copyright) into the public domain under CC0.
 
+2026-2-1
+Modified by mzhan017 to add symbol printing feature.
+
 ## Build
 
 Run `make` or just compile it directly with your C compiler, like: `cc dump-vdso.c -o dump-vdso`.
+
+For 32bit, run `gcc -m32 dump-vdso.c -o dump-vdso-32`.
 
 ## Run
 
@@ -19,6 +24,39 @@ The raw vDSO data is saved to file `vdso.so` by default. You can also print just
 ```bash
 ./dump-vdso    # default: save to file vdso.so
 ./dump-vdso -s # just print symbols and addresses
+```
+### Example of output
+32 bit
+```bash
+mzhan017:/mnt/d/dump-vdso$ ./dump-vdso -s
+Name                                     Address      Offset
+----                                     -------      ------
+__kernel_vsyscall                        0xf7fa1560 0x00000560
+__vdso_gettimeofday                      0xf7fa1790 0x00000790
+__vdso_clock_getres                      0xf7fa2520 0x00001520
+__vdso_clock_gettime64                   0xf7fa2030 0x00001030
+__kernel_sigreturn                       0xf7fa1580 0x00000580
+__vdso_time                              0xf7fa1b40 0x00000b40
+__kernel_rt_sigreturn                    0xf7fa1590 0x00000590
+__vdso_clock_gettime                     0xf7fa1b80 0x00000b80
+__vdso_getcpu                            0xf7fa25b0 0x000015b0
+```
+64 bit
+```bash
+mzhan017:/mnt/d/dump-vdso$ ./dump-vdso -s
+Name                                     Address            Offset
+----                                     -------            ------
+clock_gettime                            0x00007fffc1f8fa70 0x00000a70
+__vdso_gettimeofday                      0x00007fffc1f8f7b0 0x000007b0
+clock_getres                             0x00007fffc1f8fdd0 0x00000dd0
+__vdso_clock_getres                      0x00007fffc1f8fdd0 0x00000dd0
+gettimeofday                             0x00007fffc1f8f7b0 0x000007b0
+__vdso_time                              0x00007fffc1f8fa40 0x00000a40
+__vdso_sgx_enter_enclave                 0x00007fffc1f8fe70 0x00000e70
+time                                     0x00007fffc1f8fa40 0x00000a40
+__vdso_clock_gettime                     0x00007fffc1f8fa70 0x00000a70
+__vdso_getcpu                            0x00007fffc1f8fe40 0x00000e40
+getcpu                                   0x00007fffc1f8fe40 0x00000e40
 ```
     
 ## Inspect
--- dump-vdso.c
+++ dump-vdso.c
@@ -51,128 +51,216 @@ int main(int argc, char **argv)
 		fprintf(stderr, "not an ELF image\n");
 		return 1;
 	}
-	if (ehdr->e_ident[EI_CLASS] != ELFCLASS64) {
-		fprintf(stderr, "only ELF64 supported by this tool\n");
-		return 1;
-	}
 
-	Elf64_Phdr *phdr = (Elf64_Phdr*)(base + ehdr->e_phoff);
-	Elf64_Dyn *dyn = NULL;
-	for (int i = 0; i < ehdr->e_phnum; ++i) {
-		if (phdr[i].p_type == PT_DYNAMIC) {
-			dyn = (Elf64_Dyn*)(base + phdr[i].p_vaddr);
-			break;
-		}
-	}
-	if (!dyn) {
-		fprintf(stderr, "no PT_DYNAMIC in vdso\n");
-		return 1;
-	}
-
-	Elf64_Sym *symtab = NULL;
-	const char *strtab = NULL;
-	uint64_t strsz = 0;
-	void *hash = NULL;
-	void *gnu_hash = NULL;
-	size_t syment = sizeof(Elf64_Sym);
-
-	for (Elf64_Dyn *d = dyn; d->d_tag != DT_NULL; ++d) {
-		switch (d->d_tag) {
-		case DT_STRTAB: strtab = (const char*)d->d_un.d_ptr; break;
-		case DT_STRSZ: strsz = d->d_un.d_val; break;
-		case DT_SYMTAB: symtab = (Elf64_Sym*)d->d_un.d_ptr; break;
-		case DT_SYMENT: syment = d->d_un.d_val; break;
-		case DT_HASH: hash = (void*)d->d_un.d_ptr; break;
-		case DT_GNU_HASH: gnu_hash = (void*)d->d_un.d_ptr; break;
-		default: break;
-		}
-	}
-
-	if (!symtab || !strtab) {
-		fprintf(stderr, "no dynamic symtab/strtab found\n");
-		return 1;
-	}
-
-	// If the DT_* pointers are runtime addresses, they should be valid pointers already.
-	// If they look like offsets (small values), convert to offsets relative to base.
 	uintptr_t b = (uintptr_t)base;
 	uintptr_t e = (uintptr_t)vdso_end;
 
-	uintptr_t symtab_addr = (uintptr_t)symtab;
-	uintptr_t strtab_addr = (uintptr_t)strtab;
-	uintptr_t hash_addr = (uintptr_t)hash;
-	uintptr_t gnu_hash_addr = (uintptr_t)gnu_hash;
-
-	if (symtab_addr < b || symtab_addr >= e) symtab_addr = b + symtab_addr;
-	if (strtab_addr < b || strtab_addr >= e) strtab_addr = b + strtab_addr;
-	if (hash_addr && (hash_addr < b || hash_addr >= e)) hash_addr = b + hash_addr;
-	if (gnu_hash_addr && (gnu_hash_addr < b || gnu_hash_addr >= e)) gnu_hash_addr = b + gnu_hash_addr;
-
-	symtab = (Elf64_Sym*)symtab_addr;
-	strtab = (const char*)strtab_addr;
-	hash = hash_addr ? (void*)hash_addr : NULL;
-	gnu_hash = gnu_hash_addr ? (void*)gnu_hash_addr : NULL;
-
-	size_t symcount = 0;
-	if (hash) {
-		uint32_t *h = (uint32_t*)hash;
-		uint32_t nbucket = h[0];
-		uint32_t nchain = h[1];
-		symcount = nchain;
-	} else if (strsz) {
-		// Best-effort: iterate symbols until symbol name index exceeds strtab size.
-		// Protect with a reasonable upper bound.
-		size_t max = 10000;
-		for (size_t i = 0; i < max; ++i) {
-			Elf64_Sym *s = (Elf64_Sym*)((char*)symtab + i * syment);
-			if ((size_t)s->st_name >= strsz) { symcount = i; break; }
-			// if last entry has st_name==0, continue; ensure we don't undercount.
-			if (i == max-1) symcount = max;
+	if (ehdr->e_ident[EI_CLASS] == ELFCLASS64) {
+		Elf64_Phdr *phdr = (Elf64_Phdr*)(base + ehdr->e_phoff);
+		Elf64_Dyn *dyn = NULL;
+		for (int i = 0; i < ehdr->e_phnum; ++i) {
+			if (phdr[i].p_type == PT_DYNAMIC) {
+				dyn = (Elf64_Dyn*)(base + phdr[i].p_vaddr);
+				break;
+			}
 		}
-	} else if (gnu_hash) {
-		// Conservative fallback: try counting until a symbol with invalid name.
-		size_t max = 10000;
-		for (size_t i = 0; i < max; ++i) {
-			Elf64_Sym *s = (Elf64_Sym*)((char*)symtab + i * syment);
-			const char *name = (const char*)( (char*)strtab + s->st_name );
-			if ((uintptr_t)name < b || (uintptr_t)name >= e) { symcount = i; break; }
-			if (i == max-1) symcount = max;
-		}
-	} else {
-		// Last fallback: assume a small number
-		symcount = 256;
-	}
-
-	if (show_symbols) {
-		printf("%-40s %-18s %s\n", "Name", "Address", "Offset");
-		printf("%-40s %-18s %s\n", "----", "-------", "------");
-
-		for (size_t i = 0; i < symcount; ++i) {
-			Elf64_Sym *s = (Elf64_Sym*)((char*)symtab + i * syment);
-			unsigned char type = ELF64_ST_TYPE(s->st_info);
-			if (type != STT_FUNC) continue;
-			const char *name = strtab + s->st_name;
-			if (!name || name[0] == '\0') continue;
-			uintptr_t val = s->st_value;
-			uintptr_t addr_val;
-			if (val >= b && val < e) addr_val = val;
-			else addr_val = b + val;
-			printf("%-40s 0x%016lx 0x%08lx\n", name, (unsigned long)addr_val, (unsigned long)(addr_val - b));
-		}
-	} else {
-		FILE *out = fopen("vdso.so", "wb");
-		if (!out) {
-			perror("fopen(vdso.so)");
+		if (!dyn) {
+			fprintf(stderr, "no PT_DYNAMIC in vdso\n");
 			return 1;
 		}
-		size_t wrote = fwrite(base, 1, vdso_size, out);
-		if (wrote != vdso_size) {
-			perror("fwrite(vdso.so)");
+
+		Elf64_Sym *symtab = NULL;
+		const char *strtab = NULL;
+		uint64_t strsz = 0;
+		void *hash = NULL;
+		void *gnu_hash = NULL;
+		size_t syment = sizeof(Elf64_Sym);
+
+		for (Elf64_Dyn *d = dyn; d->d_tag != DT_NULL; ++d) {
+			switch (d->d_tag) {
+			case DT_STRTAB: strtab = (const char*)d->d_un.d_ptr; break;
+			case DT_STRSZ: strsz = d->d_un.d_val; break;
+			case DT_SYMTAB: symtab = (Elf64_Sym*)d->d_un.d_ptr; break;
+			case DT_SYMENT: syment = d->d_un.d_val; break;
+			case DT_HASH: hash = (void*)d->d_un.d_ptr; break;
+			case DT_GNU_HASH: gnu_hash = (void*)d->d_un.d_ptr; break;
+			default: break;
+			}
+		}
+
+		if (!symtab || !strtab) {
+			fprintf(stderr, "no dynamic symtab/strtab found\n");
+			return 1;
+		}
+
+		uintptr_t symtab_addr = (uintptr_t)symtab;
+		uintptr_t strtab_addr = (uintptr_t)strtab;
+		uintptr_t hash_addr = (uintptr_t)hash;
+		uintptr_t gnu_hash_addr = (uintptr_t)gnu_hash;
+
+		if (symtab_addr < b || symtab_addr >= e) symtab_addr = b + symtab_addr;
+		if (strtab_addr < b || strtab_addr >= e) strtab_addr = b + strtab_addr;
+		if (hash_addr && (hash_addr < b || hash_addr >= e)) hash_addr = b + hash_addr;
+		if (gnu_hash_addr && (gnu_hash_addr < b || gnu_hash_addr >= e)) gnu_hash_addr = b + gnu_hash_addr;
+
+		symtab = (Elf64_Sym*)symtab_addr;
+		strtab = (const char*)strtab_addr;
+		hash = hash_addr ? (void*)hash_addr : NULL;
+		gnu_hash = gnu_hash_addr ? (void*)gnu_hash_addr : NULL;
+
+		size_t symcount = 0;
+		if (hash) {
+			uint32_t *h = (uint32_t*)hash;
+			uint32_t nchain = h[1];
+			symcount = nchain;
+		} else if (strsz) {
+			size_t max = 10000;
+			for (size_t i = 0; i < max; ++i) {
+				Elf64_Sym *s = (Elf64_Sym*)((char*)symtab + i * syment);
+				if ((size_t)s->st_name >= strsz) { symcount = i; break; }
+				if (i == max-1) symcount = max;
+			}
+		} else if (gnu_hash) {
+			size_t max = 10000;
+			for (size_t i = 0; i < max; ++i) {
+				Elf64_Sym *s = (Elf64_Sym*)((char*)symtab + i * syment);
+				const char *name = (const char*)( (char*)strtab + s->st_name );
+				if ((uintptr_t)name < b || (uintptr_t)name >= e) { symcount = i; break; }
+				if (i == max-1) symcount = max;
+			}
+		} else {
+			symcount = 256;
+		}
+
+		if (show_symbols) {
+			printf("%-40s %-18s %s\n", "Name", "Address", "Offset");
+			printf("%-40s %-18s %s\n", "----", "-------", "------");
+
+			for (size_t i = 0; i < symcount; ++i) {
+				Elf64_Sym *s = (Elf64_Sym*)((char*)symtab + i * syment);
+				unsigned char type = ELF64_ST_TYPE(s->st_info);
+				if (type != STT_FUNC) continue;
+				const char *name = strtab + s->st_name;
+				if (!name || name[0] == '\0') continue;
+				uintptr_t val = s->st_value;
+				uintptr_t addr_val;
+				if (val >= b && val < e) addr_val = val;
+				else addr_val = b + val;
+				printf("%-40s 0x%016lx 0x%08lx\n", name, (unsigned long)addr_val, (unsigned long)(addr_val - b));
+			}
+		} else {
+			FILE *out = fopen("vdso.so", "wb");
+			if (!out) { perror("fopen(vdso.so)"); return 1; }
+			size_t wrote = fwrite(base, 1, vdso_size, out);
+			if (wrote != vdso_size) { perror("fwrite(vdso.so)"); fclose(out); return 1; }
 			fclose(out);
+			printf("wrote vdso.so (%zu bytes)\n", vdso_size);
+		}
+	} else if (ehdr->e_ident[EI_CLASS] == ELFCLASS32) {
+		Elf32_Ehdr *eh32 = (Elf32_Ehdr*)base;
+		Elf32_Phdr *ph32 = (Elf32_Phdr*)(base + eh32->e_phoff);
+		Elf32_Dyn *dyn32 = NULL;
+		for (int i = 0; i < eh32->e_phnum; ++i) {
+			if (ph32[i].p_type == PT_DYNAMIC) {
+				dyn32 = (Elf32_Dyn*)(base + ph32[i].p_vaddr);
+				break;
+			}
+		}
+		if (!dyn32) {
+			fprintf(stderr, "no PT_DYNAMIC in vdso (32-bit)\n");
 			return 1;
 		}
-		fclose(out);
-		printf("wrote vdso.so (%zu bytes)\n", vdso_size);
+
+		Elf32_Sym *symtab32 = NULL;
+		const char *strtab32 = NULL;
+		uint32_t strsz32 = 0;
+		void *hash32 = NULL;
+		void *gnu_hash32 = NULL;
+		size_t syment32 = sizeof(Elf32_Sym);
+
+		for (Elf32_Dyn *d = dyn32; d->d_tag != DT_NULL; ++d) {
+			switch (d->d_tag) {
+			case DT_STRTAB: strtab32 = (const char*)d->d_un.d_ptr; break;
+			case DT_STRSZ: strsz32 = d->d_un.d_val; break;
+			case DT_SYMTAB: symtab32 = (Elf32_Sym*)d->d_un.d_ptr; break;
+			case DT_SYMENT: syment32 = d->d_un.d_val; break;
+			case DT_HASH: hash32 = (void*)d->d_un.d_ptr; break;
+			case DT_GNU_HASH: gnu_hash32 = (void*)d->d_un.d_ptr; break;
+			default: break;
+			}
+		}
+
+		if (!symtab32 || !strtab32) {
+			fprintf(stderr, "no dynamic symtab/strtab found (32-bit)\n");
+			return 1;
+		}
+
+		uintptr_t symtab_addr = (uintptr_t)symtab32;
+		uintptr_t strtab_addr = (uintptr_t)strtab32;
+		uintptr_t hash_addr = (uintptr_t)hash32;
+		uintptr_t gnu_hash_addr = (uintptr_t)gnu_hash32;
+
+		if (symtab_addr < b || symtab_addr >= e) symtab_addr = b + symtab_addr;
+		if (strtab_addr < b || strtab_addr >= e) strtab_addr = b + strtab_addr;
+		if (hash_addr && (hash_addr < b || hash_addr >= e)) hash_addr = b + hash_addr;
+		if (gnu_hash_addr && (gnu_hash_addr < b || gnu_hash_addr >= e)) gnu_hash_addr = b + gnu_hash_addr;
+
+		symtab32 = (Elf32_Sym*)symtab_addr;
+		strtab32 = (const char*)strtab_addr;
+		hash32 = hash_addr ? (void*)hash_addr : NULL;
+		gnu_hash32 = gnu_hash_addr ? (void*)gnu_hash_addr : NULL;
+
+		size_t symcount = 0;
+		if (hash32) {
+			uint32_t *h = (uint32_t*)hash32;
+			uint32_t nchain = h[1];
+			symcount = nchain;
+		} else if (strsz32) {
+			size_t max = 10000;
+			for (size_t i = 0; i < max; ++i) {
+				Elf32_Sym *s = (Elf32_Sym*)((char*)symtab32 + i * syment32);
+				if ((size_t)s->st_name >= strsz32) { symcount = i; break; }
+				if (i == max-1) symcount = max;
+			}
+		} else if (gnu_hash32) {
+			size_t max = 10000;
+			for (size_t i = 0; i < max; ++i) {
+				Elf32_Sym *s = (Elf32_Sym*)((char*)symtab32 + i * syment32);
+				const char *name = (const char*)( (char*)strtab32 + s->st_name );
+				if ((uintptr_t)name < b || (uintptr_t)name >= e) { symcount = i; break; }
+				if (i == max-1) symcount = max;
+			}
+		} else {
+			symcount = 256;
+		}
+
+		if (show_symbols) {
+			printf("%-40s %-12s %s\n", "Name", "Address", "Offset");
+			printf("%-40s %-12s %s\n", "----", "-------", "------");
+
+			for (size_t i = 0; i < symcount; ++i) {
+				Elf32_Sym *s = (Elf32_Sym*)((char*)symtab32 + i * syment32);
+				unsigned char type = ELF32_ST_TYPE(s->st_info);
+				if (type != STT_FUNC) continue;
+				const char *name = strtab32 + s->st_name;
+				if (!name || name[0] == '\0') continue;
+				uint32_t val = s->st_value;
+				uintptr_t addr_val;
+				if ((uintptr_t)val >= b && (uintptr_t)val < e) addr_val = (uintptr_t)val;
+				else addr_val = b + (uintptr_t)val;
+				printf("%-40s 0x%08lx 0x%08lx\n", name, (unsigned long)addr_val, (unsigned long)(addr_val - b));
+			}
+		} else {
+			FILE *out = fopen("vdso.so", "wb");
+			if (!out) { perror("fopen(vdso.so)"); return 1; }
+			size_t wrote = fwrite(base, 1, vdso_size, out);
+			if (wrote != vdso_size) { perror("fwrite(vdso.so)"); fclose(out); return 1; }
+			fclose(out);
+			printf("wrote vdso.so (%zu bytes)\n", vdso_size);
+		}
+	} else {
+		fprintf(stderr, "unsupported ELF class: %d\n", ehdr->e_ident[EI_CLASS]);
+		return 1;
 	}
 
 	return 0;
